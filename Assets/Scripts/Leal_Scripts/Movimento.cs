f4ab61f6feada96a9ba2a48bd56b5359d313faff
Kaique Leal <leal.k.2199@gmail.com>
2025-09-11 17:25:33 -0300

    jogador adicionado

--- Assets/Scripts/Leal_Scripts/Movimento.cs
+++ Assets/Scripts/Leal_Scripts/Movimento.cs
@@ -6,17 +6,27 @@ public class Movimento : MonoBehaviour
 {
     public float velocidadePlayer = 1;
     public float velocidadeHorizontal = 3;
+    public float limiteDireito = 4f;
+    public float limiteEsquerdo = 4f;
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * velocidadePlayer, Space.World);
+
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * velocidadeHorizontal);
+            if (this.gameObject.transform.position.x > (limiteEsquerdo * -1))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * velocidadeHorizontal);
+            }
         }
+
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * velocidadeHorizontal);
+            if (this.gameObject.transform.position.x < limiteDireito)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * velocidadeHorizontal);
+            }
         }
     }
 }
