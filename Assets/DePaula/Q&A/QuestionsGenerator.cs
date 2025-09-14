80334d46bc077229c6e7b7112e74b141b6dfaa64
Pedro de Paula <pedrodepaula2005@gmail.com>
2025-09-14 17:01:17 -0300

    Question Generator

---
+++ Assets/DePaula/Q&A/QuestionsGenerator.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
+public class QuestionsGenerator : MonoBehaviour
+{
+    [Header("TextComponents")]
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] List<TextMeshProUGUI> answersText;
+    [Space]
+    [Header("Databases")]
+    public List<QuestionDatabaseConfig> databases;
+
+    int difficultyLevel = 0;
+    Subject currentSubject = Subject.Portuguese;
+
+    QuestionAnswer currentQuestion;
+
+    private void Start()
+    {
+        currentSubject = (Subject)PlayerPrefs.GetInt("Subject", 1);
+    }
+
+    private QuestionAnswer GetRandomItem()
+    {
+        foreach (var item in databases)
+        {
+            if (item.subject == currentSubject && item.level == difficultyLevel)
+            {
+                return item.database.GetRandomItem();
+            }
+        }
+
+        Debug.LogError("Can't find suitable database to get item from!");
+        return null;
+    }
+
+    public void GenerateQuestionAnswer()
+    {
+        currentQuestion = GetRandomItem();
+
+        if (currentQuestion == null) return;
+
+        questionText.text = currentQuestion.question;
+
+        List<string> array = currentQuestion.GetRandomizedArray();
+
+        for(int i = 0; i < array.Count; i++)
+        {
+            answersText[i].text = "A) " + array[i];
+        }
+    }
+
+    public void ChooseAnswer(string answer)
+    {
+        if (currentQuestion == null) return;
+
+        if (currentQuestion.TestAnswer(answer))
+        {
+            ScoreManager.instance.ScoreUp();
+        }
+        else
+        {
+            ScoreManager.instance.LoseLife();
+        }
+    }
+}
+
+
+[System.Serializable]
+public class QuestionDatabaseConfig
+{
+    public QuestionDatabase database;
+    public Subject subject;
+    public int level;
+}
